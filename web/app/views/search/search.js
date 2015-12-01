1d686a05563dee51d6d9101c78407da008082c43
Sebastian <sebnielsen@hotmail.com>
2015-12-01 20:45:40 +0100

    front end modified

--- web/app/views/search/search.js
+++ web/app/views/search/search.js
@@ -8,79 +8,76 @@ angular.module('myApp.Search', ['ngRoute'])
                 });
             }])
 
-        .controller('SearchCtrl', ['TravelFactory', function (TravelFactory) {
+        .controller('SearchCtrl', ['SearchFactory', 'AirportFactory', function (SearchFactory, AirportFactory, $rootScope) {
                 var self = this;
-                self.airports = TravelFactory.getAirports();
+                self.airports = AirportFactory.getAirports();
+                self.results = {};
                 self.ShowResults = false;
                 self.searchRequest = {};
-                self.search = function (){
-                    if(self.searchRequest.destination === undefined){
-                        TravelFactory.getAllFlightsFromOrigin(self.searchRequest);
+                self.search = function () {
+                    if (self.searchRequest.destination === undefined) {
+                        SearchFactory.getAllFlightsFromOrigin(self.searchRequest)
+                            .success(function (results) {
+                                self.results = results;
+                                self.ShowResults = true;
+                            })
+                            .error(function (data) {
+                                $rootScope.error = data.error + " : " + data.message;
+                            });
                     }
                     else {
-                        TravelFactory.getAllFlightsFromOriginToDestination(self.searchRequest);
+                        SearchFactory.getAllFlightsFromOriginToDestination(self.searchRequest)
+                            .success(function (results) {
+                                self.results = results;
+                                self.ShowResults = true;
+                            })
+                            .error(function (data) {
+                                $rootScope.error = data.error + " : " + data.message;
+                            });
                     }
-                }
+                };
 
             }])
 
-        .factory('TravelFactory', ['$http', function ($http) {
+        .factory('AirportFactory', ['$http', function ($http) {
                 var airports = [{IATACODE: "CPH", country: "Denmark", city: "Copenhagen"},
-                    {IATACODE: "LHR", country: "England", city: "London"},
+                    {IATACODE: "STN", country: "England", city: "London"},
                     {IATACODE: "BCN", country: "Spain", city: "Barcelona"},
                     {IATACODE: "MIL", country: "Italy", city: "Milano"}
                 ];
-                
-                var flights = [];
-                
+
                 var getAirports = function () {
                     return airports;
                 };
                 
-                var getFlights = function () {
-                    return flights;
-                };  
-                
-                var getAllFlightsFromOrigin = function(searchRequest) {
+                return {
+                    getAirports: getAirports  
+                };
+            }])
+
+        .factory('SearchFactory', ['$http', function ($http, $rootScope) {
+                var getAllFlightsFromOrigin = function (searchRequest) {
                     var origin = parseIATACode(searchRequest.origin);
                     var url = "api/flightinfo/" + origin + "/" + searchRequest.departing.toISOString() + "/" + searchRequest.numberOfTickets;
-                    $http.get(url)
-                            .success(function (data) {
-                                flights.length = 0;
-                                console.log(url);
-//                                data.forEach(function(flight) {
-//                                      console.log(flight)
-////                                    flights.push(flight);
-//                                });                             
-                            })
-                            .error(function (data) {
-                                $rootScope.error = data.error + " : " + data.message;
-                            });
+                    return $http.get(url);
                 };
-                
-                var getAllFlightsFromOriginToDestination = function(searchRequest) {
+
+                var getAllFlightsFromOriginToDestination = function (searchRequest) {
                     var origin = parseIATACode(searchRequest.origin);
                     var destination = parseIATACode(searchRequest.destination);
                     var url = "api/flightinfo/" + origin + "/" + destination + "/" + searchRequest.departing.toISOString() + "/" + searchRequest.numberOfTickets;
-                    $http.get(url)
-                            .success(function (data) {
-                            })
-                            .error(function (data) {
-                                $rootScope.error = data.error + " : " + data.message;
-                            });
+                    return $http.get(url);
+                };    
+
+                var parseIATACode = function (Airport) {
+                    var partsArray = Airport.split(' ');
+                    return partsArray[1].substring(1, 4);
                 };
                 
-                var parseIATACode = function(Airport){
-                    var partsArray = Airport.split(' ');
-                    return partsArray[1].substring(1,4);
-                }
                 return {
-                    getAirports: getAirports,
                     getAllFlightsFromOrigin: getAllFlightsFromOrigin,
                     getAllFlightsFromOriginToDestination: getAllFlightsFromOriginToDestination
                 };
-                
+
             }]);
-
-
-
+        
