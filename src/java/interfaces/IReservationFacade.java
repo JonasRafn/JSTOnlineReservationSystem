f8a3b0ae44e81e3cc98620fa099411e2fea5a7ae
JonasRafn <jonaschrafn@gmail.com>
2015-12-08 13:22:26 +0100

    Most basic backend of reservation implemented

--- src/java/interfaces/IReservationFacade.java
+++ src/java/interfaces/IReservationFacade.java
@@ -1,10 +1,14 @@
 package interfaces;
 
 import entity.Reservation;
+import exception.ServerException;
+import java.io.IOException;
 import java.util.List;
 
 public interface IReservationFacade {
-    
+
     public List<Reservation> getReservations(String username);
-    
+
+    public Reservation reservateTickets(String reservation, String groupName, String user) throws IOException, ServerException;
+
 }
---
+++ test/facadetest/ReservationFacadeTest.java
@@ -0,0 +1,68 @@
+package facadetest;
+
+import deploy.DeploymentConfiguration;
+import entity.Reservation;
+import exception.ServerException;
+import facades.ReservationFacade;
+import interfaces.IReservationFacade;
+import java.io.IOException;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ReservationFacadeTest {
+
+    private IReservationFacade rf;
+
+    public ReservationFacadeTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void resevateTicketsSucces() throws IOException, ServerException {
+        rf = new ReservationFacade();
+        String reservation = "{\n"
+                + " \"flightID\": \"COL3257x100x2016-01-14T21:30:00.000Z\",\n"
+                + " \"numberOfSeats\": 2,\n"
+                + " \"ReserveeName\": \"Peter Hansen\",\n"
+                + " \"ReservePhone\": \"12345678\",\n"
+                + " \"ReserveeEmail\": \"peter@peter.dk\",\n"
+                + " \"Passengers\":[\n"
+                + " { \"firstName\":\"Peter\",\n"
+                + " \"lastName\": \"Peterson\"\n"
+                + " },\n"
+                + " { \"firstName\":\"Jane\",\n"
+                + " \"lastName\": \"Peterson\"\n"
+                + " }\n"
+                + " ]\n"
+                + "}";
+
+        Reservation res = rf.reservateTickets(reservation, "angular_airline1", "user");
+
+        assertEquals(res.getFlightID(), "COL3257x100x2016-01-14T21:30:00.000Z");
+        assertEquals("Peter Hansen", res.getReserveeName());
+        assertEquals(90, res.getTraveltime());
+
+    }
+}
