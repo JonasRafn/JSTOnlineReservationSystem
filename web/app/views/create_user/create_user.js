1548a93c7e3e6aec753da2a31cdf4ee1ef5b5cd1
JonasRafn <jonaschrafn@gmail.com>
2015-12-17 14:26:45 +0100

    Changed create user link to a button. Create_user now redirects to search.

--- web/app/views/create_user/create_user.js
+++ web/app/views/create_user/create_user.js
@@ -9,21 +9,24 @@ angular.module('myApp.CreateUser', ['ngRoute'])
                 });
             }])
 
-        .controller('CreateUserCtrl', function ($http, $rootScope) {
-            var self = this;
-            self.user = {};
-            self.createUser = function () {
-                if (self.user.userName === undefined) {
-                    $rootScope.error = "You must input a username!";
-                } else if (self.user.password === undefined) {
-                    $rootScope.error = "You must input a password!";
-                } else {
-                    $http.post("api/create", self.user)
-                            .then(function (response) {
-                                $rootScope.success = "User " + response.data.userName + " succesfullly created!";
-                            }, function (response) {
-                                $rootScope.error = response.data.message;
-                            });
-                }
-            };
-        });
+        .controller('CreateUserCtrl', ['$http', '$rootScope', '$location', '$timeout', function ($http, $rootScope, $location, $timeout) {
+                var self = this;
+                self.user = {};
+                self.createUser = function () {
+                    if (self.user.userName === undefined) {
+                        $rootScope.error = "You must input a username!";
+                    } else if (self.user.password === undefined) {
+                        $rootScope.error = "You must input a password!";
+                    } else {
+                        $http.post("api/create", self.user)
+                                .then(function (response) {
+                                    $rootScope.success = "User " + response.data.userName + " succesfullly created!";
+                                    $timeout(function () {
+                                        $location.path("/search");
+                                    }, 1500);
+                                }, function (response) {
+                                    $rootScope.error = response.data.message;
+                                });
+                    }
+                };
+            }]);
